8520e0de3b19aa145bf5b18a33c896fd5786d0eb
dzmarshall <dmarshall@webonise.com>
2015-01-20 09:14:23 -0500

    Fix login permission issue in tests

--- test/functional/project_configurations_controller_test.rb
+++ test/functional/project_configurations_controller_test.rb
@@ -5,19 +5,19 @@ class ProjectConfigurationsControllerTest < ActionController::TestCase
   def setup
     @project = create_new_project
     @config = create_new_project_configuration(:project => @project)
-    @user = login
+    @user = admin_login
   end
 
   def test_should_get_new
     get :new, :project_id => @project.id
     assert_response :success
   end
-  
+
   def test_should_create_project_configuration
-    old_count = ProjectConfiguration.count
-    post :create, :project_id => @project.id, :configuration => { :name => 'a', :value => 'b' }
-    assert_equal old_count+1, ProjectConfiguration.count
-    
+    assert_difference 'ProjectConfiguration.count', 1 do
+      post :create, :project_id => @project.id, :configuration => { :name => 'a', :value => 'b' }
+    end
+
     assert_redirected_to project_path(@project)
   end
 
@@ -25,17 +25,17 @@ class ProjectConfigurationsControllerTest < ActionController::TestCase
     get :edit, :project_id => @project.id, :id => @config.id
     assert_response :success
   end
-  
+
   def test_should_update_project_configuration
     put :update, :project_id => @project.id, :id => @config.id, :configuration => { :name => 'a', :value => 'b'}
     assert_redirected_to project_path(@project)
   end
-  
+
   def test_should_destroy_project_configuration
-    old_count = ProjectConfiguration.count
-    delete :destroy, :project_id => @project.id, :id => @config.id
-    assert_equal old_count-1, ProjectConfiguration.count
-    
+    assert_difference 'ProjectConfiguration.count', -1 do
+      delete :destroy, :project_id => @project.id, :id => @config.id
+    end
+
     assert_redirected_to project_path(@project)
   end
 end
