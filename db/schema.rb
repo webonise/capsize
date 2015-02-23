3ff8e29c79ea25426dc824b3ef3bf780dc4ca090
dzmarshall <dmarshall@webonise.com>
2015-02-23 10:14:01 -0500

    Add default value, callback for extensions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150217151142) do
+ActiveRecord::Schema.define(:version => 20150223141212) do
 
   create_table "auth_sources", :force => true do |t|
     t.string   "type",              :limit => 30, :default => "",    :null => false
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(:version => 20150217151142) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "archived",    :default => false
-    t.string   "extensions"
+    t.string   "extensions",  :default => "--- []\n"
   end
 
   create_table "recipe_versions", :force => true do |t|
